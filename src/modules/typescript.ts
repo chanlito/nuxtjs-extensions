94808d7c3ffa214790bd6b5a4e84739ffda03c96
Bruce Hem <bruce.hem@pathmazing.com>
2018-01-13 13:41:14 +0700

    first commit

---
+++ src/modules/typescript.ts
@@ -0,0 +1,41 @@
+import ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = function() {
+  // Add .ts extension for store, middleware and more
+  this.nuxt.options.extensions.push('ts');
+  // Extend build
+  this.extendBuild(config => {
+    const tsLoader = {
+      loader: 'ts-loader',
+      options: {
+        appendTsSuffixTo: [/\.vue$/],
+        transpileOnly: true
+      }
+    };
+    // Add TypeScript loader
+    config.module.rules.push({
+      test: /((client|server)\.js)|(\.tsx?)$/,
+      exclude: [/server/, /node_modules/],
+      ...tsLoader
+    });
+    // Add TypeScript loader for vue files
+    for (const rule of config.module.rules) {
+      if (rule.loader === 'vue-loader') {
+        rule.options.loaders.ts = tsLoader;
+      }
+    }
+    // Add .ts extension in webpack resolve
+    if (config.resolve.extensions.indexOf('.ts') === -1) {
+      config.resolve.extensions.push('.ts');
+    }
+    // Add a webpack plugin
+    config.plugins.push(
+      new ForkTsCheckerWebpackPlugin({
+        vue: true,
+        watch: ['client']
+      })
+    );
+  });
+};
+
+module.exports.meta = require('../../package.json');
